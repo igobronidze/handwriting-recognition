bd017495c54c48b4c4fc18e264dbc0bc0e432f5a
Iosebi Gobronidze <ioseb.gobronidze@azry.com>
2016-12-28 16:19:55 +0400

    tmp commit...

--- src/main/java/ge/tsu/handwritin_recognition/service/InputDataServiceImpl.java
+++ src/main/java/ge/tsu/handwritin_recognition/service/InputDataServiceImpl.java
@@ -4,6 +4,13 @@ import ge.tsu.handwritin_recognition.model.InputData;
 import ge.tsu.handwritin_recognition.systemsetting.SystemParameter;
 import org.neuroph.core.data.DataSetRow;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputDataServiceImpl implements InputDataService {
 
     @Override
@@ -17,5 +24,24 @@ public class InputDataServiceImpl implements InputDataService {
         double[] ans = new double[SystemParameter.charsSet.getNumberOfChars()];
         ans[inputData.getLetter() - SystemParameter.charsSet.getFirstCharASCI()] = 1;
         return new DataSetRow(input, ans);
+    }
+
+    @Override
+    public List<InputData> getInputDatas(String folderName) {
+        List<InputData> inputDataList = new ArrayList<>();
+        String path = SystemParameter.testDataPath + "/" + folderName;
+        File folder = new File(path);
+        for (File file : folder.listFiles()) {
+            if (!file.isDirectory()) {
+                try {
+                    ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+                    InputData inputData = (InputData)in.readObject();
+                    inputDataList.add(inputData);
+                } catch (IOException | ClassNotFoundException ex) {
+                    System.out.println("Something wrong on file " + file.getAbsolutePath());
+                }
+            }
+        }
+        return inputDataList;
     }
 }
