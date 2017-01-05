8f17b86703330e4ae5504ea651686a6343d757ff
Ioseb Gobronidze <gobronidze.ioseb@gmail.com>
2017-01-05 10:21:28 +0400

    [#26] finished integration of data_creator and neural_network...

---
+++ neural_network/src/main/java/ge/tsu/handwriting_recognition/neural_network/neural/NeuralNetworkParameter.java
@@ -0,0 +1,103 @@
+package ge.tsu.handwriting_recognition.neural_network.neural;
+
+import ge.tsu.handwriting_recognition.neural_network.transfer.SigmoidFunction;
+import ge.tsu.handwriting_recognition.neural_network.transfer.TransferFunction;
+import ge.tsu.handwriting_recognition.neural_network.transfer.TransferFunctionType;
+
+import java.io.Serializable;
+
+public class NeuralNetworkParameter implements Serializable {
+
+    private float weightMinValue = -0.5f;
+
+    private float weightMaxValue = 0.5f;
+
+    private float biasMinValue = -0.5f;
+
+    private float biasMaxValue = 0.5f;
+
+    private TransferFunction transferFunction = new SigmoidFunction();
+
+    private float learningRate = 0.2f;
+
+    private float minError = 0.005f;
+
+    private long trainingMaxIteration = 10_000_000L;
+
+    private long numberOfTrainingDataInOneIteration = 10_000_000L;
+
+    public float getWeightMinValue() {
+        return weightMinValue;
+    }
+
+    public void setWeightMinValue(float weightMinValue) {
+        this.weightMinValue = weightMinValue;
+    }
+
+    public float getWeightMaxValue() {
+        return weightMaxValue;
+    }
+
+    public void setWeightMaxValue(float weightMaxValue) {
+        this.weightMaxValue = weightMaxValue;
+    }
+
+    public float getBiasMinValue() {
+        return biasMinValue;
+    }
+
+    public void setBiasMinValue(float biasMinValue) {
+        this.biasMinValue = biasMinValue;
+    }
+
+    public float getBiasMaxValue() {
+        return biasMaxValue;
+    }
+
+    public void setBiasMaxValue(float biasMaxValue) {
+        this.biasMaxValue = biasMaxValue;
+    }
+
+    public TransferFunction getTransferFunction() {
+        return transferFunction;
+    }
+
+    public float getLearningRate() {
+        return learningRate;
+    }
+
+    public void setLearningRate(float learningRate) {
+        this.learningRate = learningRate;
+    }
+
+    public float getMinError() {
+        return minError;
+    }
+
+    public void setMinError(float minError) {
+        this.minError = minError;
+    }
+
+    public long getTrainingMaxIteration() {
+        return trainingMaxIteration;
+    }
+
+    public void setTrainingMaxIteration(long trainingMaxIteration) {
+        this.trainingMaxIteration = trainingMaxIteration;
+    }
+
+    public long getNumberOfTrainingDataInOneIteration() {
+        return numberOfTrainingDataInOneIteration;
+    }
+
+    public void setNumberOfTrainingDataInOneIteration(long numberOfTrainingDataInOneIteration) {
+        this.numberOfTrainingDataInOneIteration = numberOfTrainingDataInOneIteration;
+    }
+
+    public void setTransferFunction(TransferFunctionType type) {
+        switch (type) {
+            case SIGMOID:
+                transferFunction = new SigmoidFunction();
+        }
+    }
+}
